62d8f7b3f0bbc3f69e7d24141723c4642e8146f4
phoenixtype <samuelakuma130@gmail.com>
2021-02-02 00:33:27 -0500

    login page backend

---
+++ spring-login/src/main/java/com/RMP/springlogin/SpringLoginApplication.java
@@ -0,0 +1,13 @@
+package com.RMP.springlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginApplication.class, args);
+	}
+
+}
